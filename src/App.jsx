92a72def885bba541bf84703a31e5dd1f60b2239
royallsilwallz <royalsilwal@gmail.com>
2022-01-08 09:58:09 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Hello React!</h1>
+      <p>Hello people! This is react</p>
+    </div>
+  );
+}
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js',
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
