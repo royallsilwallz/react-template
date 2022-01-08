8c586ca1691a9f851fccbae6d60dee1423cf3755
royallsilwallz <royalsilwal@gmail.com>
2022-01-08 13:04:06 +0545

    configure redux-toolkit

---
+++ src/slices/counter.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+const incrementFunc = state => {
+  state.value += 1;
+};
+
+const decrementFunc = state => {
+  state.value -= 1;
+};
+
+const incrementByAmountFunc = (state, action) => {
+  state.value += action.payload;
+};
+
+export const slice = createSlice({
+  name: 'counter',
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: incrementFunc,
+    decrement: decrementFunc,
+    incrementByAmount: incrementByAmountFunc,
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = slice.actions;
+
+export const incrementAsync = amount => dispatch => {
+  setTimeout(() => {
+    dispatch(incrementByAmount(amount));
+  }, 1000);
+};
+
+export const selectCount = state => state.counter.value;
+
+export default slice.reducer;
